162d6cc57c8ad294681ef01c283c18b6fe255c24
Kerime Burcu Karatas <kerimeburcu@gmail.com>
2022-01-13 00:24:24 +0300

    Result yapısı oluşturuldu.

---
+++ ProgrammersBlog.Shared/Utilities/Results/Abstract/IResult.cs
@@ -0,0 +1,18 @@
+﻿using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Utilities.Results.Abstract
+{
+    //kullanıcılara sonuç dönmek için;
+    //sonuç olduğu için sadece get
+    public interface IResult
+    {
+        public ResultStatus ResultStatus { get;  } //ResultStatus.Success //ResultStatus.Error
+        public string Message { get;  }
+        public Exception Exception { get;  }
+    }
+}
